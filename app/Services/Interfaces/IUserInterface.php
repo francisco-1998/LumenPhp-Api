7aa351c6cd9b316f619a32fb51c3dab0fef0c1b5
francisco-1998 <francmarinc@gmail.com>
2021-01-10 11:07:01 -0500

    Finalizada la configuración del proyecto de Lumen

---
+++ app/Services/Interfaces/IUserInterface.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace App\Services\Interfaces;
+
+interface IUser {
+    /**
+     * @return array User
+     */
+    function getUsers();
+
+    /**
+     * @param int $id
+     * @return User
+     */
+    function getUserById(int $id);
+
+    /**
+     * @param array $user
+     * @return void
+     */
+    function addUser(array $user);
+
+    /**
+     * @param array $user
+     * @param array $id
+     * @return void
+     */
+    function updateUser(array $user, int $id);
+
+    /**
+     * @param int $id
+     * @return boolean
+     */
+    function deleteUser(int $id);
+
+    /**
+     * @param int $id
+     * @return boolean
+     */
+    function restoreUser(int $id);
+}
